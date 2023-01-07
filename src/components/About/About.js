527f119e897c015142b5137efd20aa1ea580e5ec
Soufiane sejjari <81421925+soufianesejjari@users.noreply.github.com>
2023-01-07 14:38:31 +0100

    Update About.js
    changemnet dans             src="https://github.com/soufianesejjari/portfolio-react/blob/master/src/Assets/profile2.png?raw=true"
                alt="soufiane_picture"

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,8 +15,8 @@ function About() {
         <div className="SectionTitle">About Me</div>
         <div className="BigCard">
           <Image
-            src="https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/man-svgrepo-com.svg"
-            alt="man-svgrepo"
+            src="https://github.com/soufianesejjari/portfolio-react/blob/master/src/Assets/profile2.png?raw=true"
+            alt="soufiane_picture"
           />
           <div className="AboutBio">
           Hi there! My name is <strong>Soufiane Sejjari</strong>  and I am currently completing my professional bachelor's degree in big data. I am passionate about data science and all things related to data analysis. Throughout my studies, I have had the opportunity to work on projects in data analysis, web development, and mobile development, which have helped me to develop a strong foundation in these areas. In my free time, I enjoy staying up to date with the latest trends and technologies in the field. I am excited to continue learning and growing as a data professional, and I am always open to new opportunities and challenges. Thank you for visiting my portfolio!
